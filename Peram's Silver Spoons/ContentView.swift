f772b34413ba7cbdf9dcec1ac334d418ee373627
Venkata Ramana Reddy Peram <pvramana13189@gmail.com>
2024-03-15 10:35:36 -0500

    Initial Commit

---
+++ Peram's Silver Spoons/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Peram's Silver Spoons
+//
+//  Created by OSU APP CENTER on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Peram's Silver Spoons/Peram_s_Silver_SpoonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peram_s_Silver_SpoonsApp.swift
+//  Peram's Silver Spoons
+//
+//  Created by OSU APP CENTER on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Peram_s_Silver_SpoonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
